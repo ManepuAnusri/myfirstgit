3e709599a9e570e446931a2ee98991ac0fadb804
ManepuAnusri <18r01a0494@cmritonline.ac.in>
2020-09-18 20:01:00 +0530

    first commit

---
+++ src/gitdemo1/test1.java
@@ -0,0 +1,13 @@
+package gitdemo1;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello world");
+
+	}
+
+}
